f34f67c08ecfe71b212e2ef556d4ae5e8c283c5a
Ahsannaeem405 <69482382+Ahsannaeem405@users.noreply.github.com>
2021-08-24 14:08:25 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Ahsan
+                    Laravel AhsanS
                 </div>
 
                 <div class="links">
